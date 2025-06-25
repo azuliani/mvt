fbd351b5175af0eea05f465ccc7fc805f4410c61
Alexander <alexander@example.com>
2025-06-26 00:13:48 +0200

    update to python3

--- gpio_listener.py
+++ gpio_listener.py
@@ -1,11 +1,11 @@
 import RPi.GPIO as GPIO
 import time
-import urllib2
+import requests
 
 current_milli_time = lambda: int(round(time.time() * 1000))
 
 def my_callback(a):
-    print "yup!",a
+    print ("yup!",a)
 GPIO.setwarnings(True)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(26 , GPIO.IN, pull_up_down=GPIO.PUD_UP)
@@ -14,15 +14,18 @@ cnt = 0
 def fire():
     global cnt
     cnt += 1
-    print "Yup!",cnt
-    urllib2.urlopen("http://localhost:3000/push")
+    print ("Yup!",cnt)
+    try:
+        requests.get("http://localhost:3000/push")
+    except:
+        print("Request failed")
 
 debouncing = False
 debounceval = 1
 while True:
     val = GPIO.input(26)
     now = current_milli_time()
-    if debouncing and now - debouncing >= 50 and val != debounceval:
+    if debouncing and now - debouncing >= 20 and val != debounceval:
         debouncing = False
     
     if not debouncing:
