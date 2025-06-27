bfc27832ba1bfc621934a975868c1db16e15be01
Alexander <alexander@example.com>
2025-06-27 12:05:34 +0200

    better deboucne

--- gpio_interrupt.py
+++ gpio_interrupt.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import RPi.GPIO as GPIO
 import requests
+import time
 
 BUTTON_GPIO = 26
 
@@ -18,6 +19,7 @@ if __name__ == '__main__':
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(BUTTON_GPIO, GPIO.IN, pull_up_down=GPIO.PUD_UP)
     while True:
-        GPIO.wait_for_edge(BUTTON_GPIO, GPIO.FALLING, 80)
-        print(GPIO.input(BUTTON_GPIO))
-        fire()
+        time.sleep(0.02)
+        GPIO.wait_for_edge(BUTTON_GPIO, GPIO.FALLING, 50)
+        if not GPIO.input(BUTTON_GPIO):
+            fire()
